cc3c6bd527c62161333b974f888ce5714e9d1f78
moussadiagne036 <moussadiagne036@.com>
2026-01-09 00:43:15 +0100

    premier commit

---
+++ Stocks/src/app/app/components/product-list.component/product-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-list.component',
+  imports: [],
+  templateUrl: './product-list.component.html',
+  styleUrl: './product-list.component.css',
+})
+export class ProductListComponent {
+
+}
---
+++ Stocks/src/app/app/components/services/cart.service/cart.service.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cart.service',
+  imports: [],
+  templateUrl: './cart.service.html',
+  styleUrl: './cart.service.css',
+})
+export class CartService {
+
+}
